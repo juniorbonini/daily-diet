fb902bb26cc476d3ad57b9180da56b4835c31e61
juniorbonini <juniorboninia7x@gmail.com>
2025-12-31 01:59:53 -0300

    feat(storage): implement local CRUD for diet meals using AsyncStorage
    - Create centralized diet storage module
    - Implement get, create, update and remove operations
    - Ensure data immutability when updating and deleting
    - Persist meals locally using AsyncStorage

---
+++ src/storage/diet.storage.ts
@@ -0,0 +1,54 @@
+import { Diet } from "../types/diet";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const DIET_COLLECTION = "@daily-diet:diet";
+
+async function getAll(): Promise<Diet[]> {
+  const storage = await AsyncStorage.getItem(DIET_COLLECTION);
+
+  if (!storage) {
+    return [];
+  }
+
+  return JSON.parse(storage) as Diet[];
+}
+
+async function getById(id: string): Promise<Diet | undefined> {
+  const diets = await getAll();
+
+  return diets.find((diet) => diet.id === id);
+}
+
+async function create(diet: Diet): Promise<void> {
+  const diets = await getAll();
+
+  const newDiets = [...diets, diet];
+
+  await AsyncStorage.setItem(DIET_COLLECTION, JSON.stringify(newDiets));
+}
+
+async function update(updateDiet: Diet): Promise<void> {
+  const diets = await getAll();
+
+  const newDiet = diets.map((diet) =>
+    diet.id === updateDiet.id ? updateDiet : diet
+  );
+
+  return AsyncStorage.setItem(DIET_COLLECTION, JSON.stringify(newDiet));
+}
+
+async function remove(id: string): Promise<void> {
+  const diets = await getAll();
+
+  const removeDiet = diets.filter((diet) => diet.id !== id);
+
+  return AsyncStorage.setItem(DIET_COLLECTION, JSON.stringify(removeDiet));
+}
+
+export const dietStorage = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+};
---
+++ src/types/diet.ts
@@ -0,0 +1,8 @@
+export type Diet = {
+  id: string;
+  name: string;
+  description: string;
+  hour: string;
+  date: string;
+  isOnDiet: boolean;
+};
