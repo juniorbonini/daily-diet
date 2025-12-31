65639de0d532434cb915c401bc5f25d2b577b0b3
juniorbonini <juniorboninia7x@gmail.com>
2025-12-31 00:32:02 -0300

    chore: initial project setup and documentation
    - initial project structure
    - README with project goals and development roadmap
    - yarn adopted as package manager for better dependency stability

---
+++ src/theme/colorTheme.ts
@@ -0,0 +1,23 @@
+export const colors = {
+    red: {
+        'red-dark' : '#BF3B44',
+        'red-mid' : '#F3BABD',
+        'red-light' : '#F4E6E7',
+    }, 
+    green: {
+        'green-dark': '#639339',
+        'green-mid': '#CBE4B4',
+        'green-light': '#E5F0DB'
+    },
+    gray: {
+          0: '#FFFFFF',
+        100: '#FAFAFA',
+        200: '#EFF0F0',
+        300: '#DDDEDF',
+        400: '#B9BBBC',
+        500: '#5C6265',
+        600: '#333638',
+        700: '#1B1D1E',
+    },
+
+}
